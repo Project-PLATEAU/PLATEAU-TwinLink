f6d0ffd3711af313f2d09a1d0fd2773f46ae8d9c
ren <mizurenkon@gmail.com>
2023-10-07 02:05:02 +0900

    update : 解析と経路探索でパス情報HUDのウィジットを分ける. 解析の方には車/徒歩の切り替えボタンを追加

--- Source/TwinLink/Private/NavSystem/TwinLinkNavSystemEx.cpp
+++ Source/TwinLink/Private/NavSystem/TwinLinkNavSystemEx.cpp
@@ -1,4 +1,5 @@
 #include "NavSystem/TwinLinkNavSystemEx.h"
+#include "NavSystem/TwinLinkNavSystemBuildingInfo.h"
 #include "NavSystem/TwinLinkNavSystemFindPathUiInfo.h"
 bool UTwinLinkNavSystemEx::IsValidBuilding(const FTwinLinkNavSystemBuildingInfo& Info) {
     return Info.FacilityInfo != nullptr;
@@ -21,4 +22,19 @@ bool UTwinLinkNavSystemEx::TryGetBuildingInfo(const FTwinLinkNavSystemBuildingIn
     OutBuildingIconKey = Info.FacilityInfo->GetImageFileName();
     OutEntrance = GetFirstEntranceOrZero(Info);
     return true;
+}
+
+FString UTwinLinkNavSystemEx::GetTimeText(const FTwinLinkNavSystemFindPathUiInfo& Info) {
+    return FString::Printf(TEXT("%d分"), static_cast<int>(Info.TimeSec / 60));
+}
+
+FString UTwinLinkNavSystemEx::GetDistanceTest(const FTwinLinkNavSystemFindPathUiInfo& Info) {
+    return FString::Printf(TEXT("%dm"), static_cast<int>(Info.LengthMeter));
+}
+
+void UTwinLinkNavSystemEx::GetTimeAndDistanceText(const FTwinLinkNavSystemFindPathUiInfo& Info, FString& OutTimeText,
+    FString& OutDistanceText)
+{
+    OutTimeText = GetTimeText(Info);
+    OutDistanceText = GetDistanceTest(Info);
 }
