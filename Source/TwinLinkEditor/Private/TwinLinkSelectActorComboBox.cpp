d3d9444faadc31418a47836fe11e5fa116c2b516
Hirokazu Onishi <59717249+kumax2@users.noreply.github.com>
2023-10-30 16:15:26 +0900

    feature/editor_menu_arrange (#62)
    * アクタ選択コンボボックスを追加
    都市モデル、管理対象建物の指定処理を追加
    頂点データアクセス許可処理を分離
    データ作成実行後にレベルの保存処理を追加
    
    * 選択するアクタをAPLATEAUInstancedCityModelにフィルタ

---
+++ Source/TwinLinkEditor/Private/TwinLinkSelectActorComboBox.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2023, MLIT Japan. All rights reserved.
+
+#include "TwinLinkSelectActorComboBox.h"
+#include "PropertyCustomizationHelpers.h"
+#include "PropertyHandle.h"
+#include "PLATEAUInstancedCityModel.h"
+
+UTwinLinkSelectActorComboBox::UTwinLinkSelectActorComboBox(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+    ModelActor = nullptr;
+
+    TSharedRef<SObjectPropertyEntryBox> EntryBox = SNew(SObjectPropertyEntryBox)
+        .AllowedClass(APLATEAUInstancedCityModel::StaticClass())
+        .ObjectPath_Lambda([this]()->FString const {
+        return ModelActor ? ModelActor->GetPathName() : TEXT("");
+            })
+        .OnObjectChanged_Lambda([this](const FAssetData& assetData) {
+                ModelActor = Cast<UObject>(assetData.GetAsset());
+            });
+            SetContent(EntryBox);
+}
