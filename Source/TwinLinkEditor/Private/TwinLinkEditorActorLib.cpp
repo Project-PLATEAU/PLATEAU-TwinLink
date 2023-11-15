483bb66bc093c0f82368562144fd406e68ff83ed
Ronnel216 <ryu.tsuji@synesthesias.jp>
2023-11-15 19:39:11 +0900

    街、LOD4のモデルにデカールを受けない設定を行う機能の実装 (#86)

---
+++ Source/TwinLinkEditor/Private/TwinLinkEditorActorLib.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright (C) 2023, MLIT Japan. All rights reserved.
+
+
+#include "TwinLinkEditorActorLib.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+#include "PLATEAUInstancedCityModel.h"
+#include "PLATEAUCityObjectGroup.h"
+
+bool UTwinLinkEditorActorLib::SetupCityModel(UObject* CityModel) {
+    if (CityModel == nullptr) {
+        return false;
+    }
+
+    APLATEAUInstancedCityModel* CastedCityModel = Cast<APLATEAUInstancedCityModel>(CityModel);
+    TArray<UActorComponent*> CityObjectGroups =
+        CastedCityModel->GetComponentsByClass(UPLATEAUCityObjectGroup::StaticClass());
+
+    const auto bIsReceivesDecals = false;
+    for (auto& ObjGroups : CityObjectGroups) {
+        UPLATEAUCityObjectGroup* CastedCityObjectGroup = Cast<UPLATEAUCityObjectGroup>(ObjGroups);
+        if (CastedCityObjectGroup == nullptr) {
+            continue;
+        }
+        const auto bIsDem = CastedCityObjectGroup->GetName().StartsWith("Dem_", ESearchCase::IgnoreCase);
+        if (bIsDem == false) {
+            CastedCityObjectGroup->SetReceivesDecals(false);
+        }
+    }
+
+    return true;
+}
---
+++ Source/TwinLinkEditor/Public/TwinLinkEditorActorLib.h
@@ -0,0 +1,26 @@
+﻿// Copyright (C) 2023, MLIT Japan. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorLevelLibrary.h"
+#include "TwinLinkEditorActorLib.generated.h"
+
+/**
+ * エディタで行うTwinLinkのアクターに向けた操作をまとめたライブラリ
+ */
+UCLASS()
+class TWINLINKEDITOR_API UTwinLinkEditorActorLib : public UEditorLevelLibrary
+{
+	GENERATED_BODY()
+
+public:
+    /**
+     * @brief APLATEAUInstancedCityModelに対して設定を行う
+     * @param CityModel APLATEAUInstancedCityModelへの参照
+     * @return 成否
+    */
+    UFUNCTION(BlueprintCallable, Category = "TwinLink")
+    static bool SetupCityModel(UObject* CityModel);
+	
+};
