ba5189458087398a3b85e5a6558e72ad5bccc3c6
Ronnel216 <ryu.tsuji@synesthesias.jp>
2023-12-11 14:52:47 +0900

    Fix/disable room click on map edit tab (#148)
    * 部屋選択を地図編集タブで利用できないように修正
    
    * 部屋クリックの基点のイベントをWorldViewerのものに変更

---
+++ Source/TwinLink/Private/TwinLinkRoomGetterBase.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (C) 2023, MLIT Japan. All rights reserved.
+
+
+#include "TwinLinkRoomGetterBase.h"
+
+UTwinLinkRoomGetterBase::UTwinLinkRoomGetterBase() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = false;
+
+    // ...
+}
+
+void UTwinLinkRoomGetterBase::BeginPlay() {
+    Super::BeginPlay();
+
+    // ...
+    check(EvOnClickedHnd.IsValid() == false);
+    const auto WorldViewer = ATwinLinkWorldViewer::GetInstance(GetWorld());
+    if (WorldViewer.IsValid()) {
+        EvOnClickedHnd = WorldViewer->EvOnClicked.AddLambda([this]() {
+            LineTraceToRoom();
+            });
+    }
+}
+
+void UTwinLinkRoomGetterBase::BeginDestroy() {
+    // ...
+
+    if (EvOnClickedHnd.IsValid()) {
+        const auto WorldViewer = ATwinLinkWorldViewer::GetInstance(GetWorld());
+        if (WorldViewer.IsValid()) {
+            WorldViewer->EvOnClicked.Remove(EvOnClickedHnd);
+        }
+    }
+
+    Super::BeginDestroy();
+}
+
+void UTwinLinkRoomGetterBase::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    // ...
+}
---
+++ Source/TwinLink/Public/TwinLinkRoomGetterBase.h
@@ -0,0 +1,39 @@
+﻿// Copyright (C) 2023, MLIT Japan. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TwinLinkSceneComponentBase.h"
+#include "TwinLinkRoomGetterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TWINLINK_API UTwinLinkRoomGetterBase : public UTwinLinkSceneComponentBase
+{
+	GENERATED_BODY()
+	
+public:
+    UTwinLinkRoomGetterBase();
+
+protected:
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    virtual void BeginDestroy() override;
+
+public:
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+    /**
+     * @brief ブループリント側のライントレース処理を実行する
+    */
+    UFUNCTION(BlueprintImplementableEvent, Category = "TwinLink")
+    void LineTraceToRoom();
+
+private:
+    FDelegateHandle EvOnClickedHnd;
+
+};
