3b33d006b0b0339258583f3b6779e52c8f47289b
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2023-12-27 16:25:47 +0900

    CSVのバージョンを引き上げ

--- Source/TwinLink/Public/TwinLinkFacilityInfoSystem.h
+++ Source/TwinLink/Public/TwinLinkFacilityInfoSystem.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2023, MLIT Japan. All rights reserved.
+﻿// Copyright (C) 2023, MLIT Japan. All rights reserved.
 
 #pragma once
 
@@ -144,7 +144,7 @@ public:
 
 private:
     /** 施設情報機能のバージョン　永続化時に組み込む **/
-    const TwinLinkSystemVersionInfo VersionInfo = TwinLinkSystemVersionInfo(0, 0, 0);
+    const TwinLinkSystemVersionInfo VersionInfo = TwinLinkSystemVersionInfo(1, 0, 0);
 
     /** 施設情報の永続化時のパス **/
     FString Filepath;
